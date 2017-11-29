1a1c38bf391e3b2a0411374a84e126db4a1c7e60
Alexander Eckert <aeckert@alaska.edu>
2017-11-29 13:04:45 -0900

    activities work now!

--- dev/tt_project/tt_project/tt_app/forms.py
+++ dev/tt_project/tt_project/tt_app/forms.py
@@ -18,12 +18,17 @@ class NewTrainingForm(forms.ModelForm):
             fields = ['date', 'time', 'location', 'comment', 'description']
 
 class NewActivityForm(forms.ModelForm):
-        date = forms.ModelChoiceField(queryset = Training.objects.all())
         time = forms.IntegerField(required=True, help_text="How long did you train in minutes?")
         sport_type = forms.ModelChoiceField(queryset = Sport.objects.all())
         method_type = forms.ModelChoiceField(queryset = Method.objects.all())
         intensity_type = forms.ModelChoiceField(queryset = Intensity.objects.all())
 
+        def __init__(self, *args, **kwargs):
+            user = kwargs.pop('user')
+            super(NewActivityForm, self).__init__(*args, **kwargs)
+            print(self.fields)
+            self.fields['training_date'].queryset = Training.objects.filter(athlete=user)
+
         class Meta:
             model = Activity
-            fields = ['date', 'time', 'sport_type', 'method_type', 'intensity_type', ]
+            fields = ['training_date', 'time', 'sport_type', 'method_type', 'intensity_type', ]
