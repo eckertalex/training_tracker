63ed359a79f246a566771514212c280f16b250e0
Alexander Eckert <aeckert@alaska.edu>
2017-11-29 23:15:31 -0900

    change a few things

--- dev/tt_project/tt_project/tt_app/forms.py
+++ dev/tt_project/tt_project/tt_app/forms.py
@@ -3,31 +3,32 @@ from django.utils.timezone import now
 from .models import *
 
 class NewTrainingForm(forms.ModelForm):
-        date = forms.DateTimeField(initial=now, help_text="When did you train?", required=True)
-        time = forms.IntegerField(required=True, help_text="How long did you train in minutes?")
-        location = forms.CharField(max_length=45, help_text="Where did you train?")
-        comment = forms.CharField(widget=forms.Textarea(
-            attrs={'rows': 5, 'placeholder': 'Type any comments in here..'}), 
-            max_length=255, help_text="The max length of this field is 255.")
-        description = forms.CharField(widget=forms.Textarea(
-            attrs={'rows': 5, 'placeholder': 'What is on your mind?'}),
-            max_length=255, help_text="The max length of this field is 255.")
+    date = forms.DateTimeField(initial=now, help_text="When did you train?")
+    time = forms.IntegerField(required=True, help_text="How long did you train in minutes?")
+    location = forms.CharField(required=False, max_length=45, help_text="Where did you train?")
+    comment = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={'rows': 5, 'placeholder': 'Type any comments in here..'}), 
+        max_length=255, help_text="The max length of this field is 255.")
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={'rows': 5, 'placeholder': 'What is on your mind?'}),
+        max_length=255, help_text="The max length of this field is 255.")
 
-        class Meta:
-            model = Training
-            fields = ['date', 'time', 'location', 'comment', 'description']
+    class Meta:
+        model = Training
+        fields = ['date', 'time', 'location', 'comment', 'description']
+
 
 class NewActivityForm(forms.ModelForm):
-        time = forms.IntegerField(required=True, help_text="How long did you train in minutes?")
-        sport_type = forms.ModelChoiceField(queryset = Sport.objects.all())
-        method_type = forms.ModelChoiceField(queryset = Method.objects.all())
-        intensity_type = forms.ModelChoiceField(queryset = Intensity.objects.all())
+    time = forms.IntegerField(help_text="How long did you train in minutes?")
+    sport_type = forms.ModelChoiceField(queryset = Sport.objects.all())
+    method_type = forms.ModelChoiceField(queryset = Method.objects.all())
+    intensity_type = forms.ModelChoiceField(queryset = Intensity.objects.all())
 
-        def __init__(self, *args, **kwargs):
-            user = kwargs.pop('user')
-            super(NewActivityForm, self).__init__(*args, **kwargs)
-            self.fields['training_date'].queryset = Training.objects.filter(athlete=user)
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(NewActivityForm, self).__init__(*args, **kwargs)
+        self.fields['training_date'].queryset = Training.objects.filter(athlete=user)
 
-        class Meta:
-            model = Activity
-            fields = ['training_date', 'time', 'sport_type', 'method_type', 'intensity_type', ]
+    class Meta:
+        model = Activity
+        fields = ['training_date', 'time', 'sport_type', 'method_type', 'intensity_type', ]
