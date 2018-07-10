ad05e187ba7ec4c2fc7b92992eac67901b754e83
Alexander Eckert <alexandereckert93@gmail.com>
2018-07-09 23:10:56 -0800

    remove whitenoise storage

--- tt_project/settings.py
+++ tt_project/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),        
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
