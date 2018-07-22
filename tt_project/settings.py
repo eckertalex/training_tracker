ed26dd9198e0d6a458b03b28738f36ba8e7ae3f3
Alexander Eckert <alexandereckert@hotmail.de>
2018-07-22 13:11:42 -0800

    move over to CDNs

--- tt_project/settings.py
+++ tt_project/settings.py
@@ -140,36 +140,3 @@ DEFAULT_FROM_EMAIL = 'Training Tracker <noreply@trainingtracker.com>'
 EMAIL_SUBJECT_PREFIX = '[Training Tracker] '
 
 LOGIN_URL = 'login'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
